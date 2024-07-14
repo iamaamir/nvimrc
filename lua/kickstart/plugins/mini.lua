b4b592bda7b84dae90f292137d488c02b4989267
iamaamir <8420386+iamaamir@users.noreply.github.com>
2024-07-14 16:30:18 +0530

    feat(mini): session management via mini.sessions

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -2,6 +2,8 @@ return {
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
     config = function()
+      -- session management on steriodsa but not so automatic
+      require('mini.sessions').setup { autoread = true }
       -- Better Around/Inside textobjects
       --
       -- Examples:
