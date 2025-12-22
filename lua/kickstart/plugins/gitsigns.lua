76a9238c2fd3216700a66266628948fed358af49
iamaamir <8420386+iamaamir@users.noreply.github.com>
2025-12-22 15:48:06 +0530

    refactor(gitsigns): keep the default symbos

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -7,13 +7,6 @@ return {
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '' },
-        change = { text = '' },
-        delete = { text = '' },
-        topdelete = { text = '' },
-        changedelete = { text = '' },
-      },
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
